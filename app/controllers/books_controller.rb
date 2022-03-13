e1a3ce1d3ad2fb59ba61f50e44400671a334a151
EC2 Default User <ec2-user@ip-172-31-83-56.ec2.internal>
2022-03-13 12:13:13 +0000

    [Finish]完成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,76 @@
+class BooksController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+       redirect_to book_path(@book), notice: 'You have created book successfully.'
+    else
+       @books = Book.all
+       @user = User.find(current_user.id)
+       render "index"
+    end
+  end
+
+  def index
+    @books = Book.all
+    @user = User.find(current_user.id)
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
+    @user = @book.user
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(@book), method: :delete
+    flash[:notice] = "Book was successfully destroyed."
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+       render "edit"
+    else
+       redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.user_id = current_user.id
+    if @book.update(book_params)
+       redirect_to book_path(@book), notice:'You have updated book successfully.'
+    else
+       render "edit"
+    end
+  end
+
+  def destroy
+      book = Book.find(params[:id])
+      book.destroy
+      redirect_to books_path
+  end
+
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
+
+end
+
+
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: "homes#top"
+  get 'home/about' => 'homes#about', as:'about'
+
+
+  resources:books,only:[:new, :create, :index, :show, :destroy, :edit, :update]
+  resources:users,only:[:index, :show, :edit, :update]
+
+end
