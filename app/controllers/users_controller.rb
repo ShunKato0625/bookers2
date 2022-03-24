ffbc144af687289fd9e6680e2adf0b1b2438ac66
ShunKato0625 <shunkato0625@gmail.com>
2022-03-24 14:31:58 +0000

    [Add]ユーザー詳細に投稿数記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @book = Book.new
     @books = @user.books
+    @today_book =  @books.created_today
+    @yesterday_book = @books.created_yesterday
+    @this_week_book = @books.created_this_week
+    @last_week_book = @books.created_last_week
   end
 
   def edit
@@ -34,7 +38,6 @@ class UsersController < ApplicationController
     end
   end
 
-  
   private
 
   def user_params
