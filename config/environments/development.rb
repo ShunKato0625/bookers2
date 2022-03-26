83c4e190329342a4cd4269ce3cd2a873de8a87a2
ShunKato0625 <shunkato0625@gmail.com>
2022-03-26 13:39:11 +0000

    [Add]オーナーイベントメール送信機能追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,16 @@ Rails.application.configure do
   config.active_job.queue_adapter = :inline
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            '-------@gmail.com',
+    password:             '---------',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
